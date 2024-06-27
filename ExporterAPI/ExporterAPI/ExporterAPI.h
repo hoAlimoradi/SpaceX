de0c076ef6a0c42a902b3571cc55f512333e1c37
hosein alimoradi <ho.alimoradi@gmail.com>
2024-06-28 02:24:07 +0330

    main application added

---
+++ ExporterAPI/ExporterAPI/ExporterAPI.h
@@ -0,0 +1,18 @@
+//
+//  ExporterAPI.h
+//  ExporterAPI
+//
+//  Created by ho on 4/8/1403 AP.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExporterAPI.
+FOUNDATION_EXPORT double ExporterAPIVersionNumber;
+
+//! Project version string for ExporterAPI.
+FOUNDATION_EXPORT const unsigned char ExporterAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExporterAPI/PublicHeader.h>
+
+
---
+++ SpaceX/SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by ho on 4/8/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
